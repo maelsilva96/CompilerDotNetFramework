391530c6295abb3a567af7a8d4a9f5b24f67ca72
Ismael Souza <ismael.souza@equals.com.br>
2021-07-31 15:51:55 -0300

    Teste

--- index.js
+++ index.js
@@ -29,36 +29,56 @@ try {
     core.addPath('C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Enterprise\\MSBuild\\Current\\bin');
     core.exportVariable('MSBuild', 'C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Enterprise\\MSBuild\\Current\\bin\\MSBuild.exe');
     
-    const resultCreateBuildFolder = await exec.exec('mkdir C:\\_build');
+    const resultCreateBuildFolder = exec.exec('mkdir C:\\_build').then((num) => {
+        console.log(num)
+    });
     console.log(`Result Create Build Folder: ${resultCreateBuildFolder}, command: mkdir mkdir C:\\_build`);
 
-    const resultCreateAppFolder = await exec.exec('mkdir C:\\App');
+    const resultCreateAppFolder = exec.exec('mkdir C:\\App').then((num) => {
+        console.log(num)
+    });
     console.log(`Result Create App Folder: ${resultCreateAppFolder}, command: mkdir C:\\App`);
 
-    const resultCopyFilesToAppFolder = await exec.exec(`|\nXcopy ${pathFolderMainProject} C:\\App /E /H /C /I`);
+    const resultCopyFilesToAppFolder = exec.exec(`|\nXcopy ${pathFolderMainProject} C:\\App /E /H /C /I`).then((num) => {
+        console.log(num)
+    });
     console.log(`Result Copy Files: ${resultCopyFilesToAppFolder}, command: \nXcopy ${pathFolderMainProject} C:\\App /E /H /C /I`);
 
-    const resultRestoreProject = await exec.exec(`nuget restore C:\\App\\${projectFilePathAndName}`);
+    const resultRestoreProject = exec.exec(`nuget restore C:\\App\\${projectFilePathAndName}`).then((num) => {
+        console.log(num)
+    });
     console.log(`Result Restore Project: ${resultRestoreProject}, command: nuget restore C:\\App\\${projectFilePathAndName}`);
 
-    const resultBuildProject = await exec.exec(`|\nMSBuild.exe /t:build /p:Configuration=${typeConfiguration} /p:Platform="${typePlatform}" ${extCommand} /p:OutDir="C:\_build" "C:\\App\\${projectFilePathAndName}"`);
+    const resultBuildProject = exec.exec(`|\nMSBuild.exe /t:build /p:Configuration=${typeConfiguration} /p:Platform="${typePlatform}" ${extCommand} /p:OutDir="C:\_build" "C:\\App\\${projectFilePathAndName}"`).then((num) => {
+        console.log(num)
+    });
     console.log(`Result Build Project: ${resultBuildProject}, command: MSBuild.exe /t:build /p:Configuration=${typeConfiguration} /p:Platform="${typePlatform}" ${extCommand} /p:OutDir="C:\_build" "C:\\App\\${projectFilePathAndName}"`);
 
     if (compactFile == true || compactFile == 'true') {
-        const resultCompress = await exec.exec(`powershell Compress-Archive C:\\_build\* .\\${nameFileCompact}`);
+        const resultCompress = exec.exec(`powershell Compress-Archive C:\\_build\* .\\${nameFileCompact}`).then((num) => {
+            console.log(num)
+        });
         console.log(`Result Compress: ${resultCompress}, command: powershell Compress-Archive C:\\_build\* .\\${nameFileCompact}`);
     } else {
-        const resultCreateBuildFolder = await exec.exec(`mkdir .\\_build`);
+        const resultCreateBuildFolder = exec.exec(`mkdir .\\_build`).then((num) => {
+            console.log(num)
+        });
         console.log(`Result Create Build Folder: ${resultCreateBuildFolder}, command: mkdir _build`);
 
-        const resultCopyFolder = await exec.exec(`|\nXcopy C:\\_build .\\_build /E /H /C /I`);
+        const resultCopyFolder = exec.exec(`|\nXcopy C:\\_build .\\_build /E /H /C /I`).then((num) => {
+            console.log(num)
+        });
         console.log(`Result Build to Build Folder: ${resultCopyFolder}, command: Xcopy C:\\_build .\\_build /E /H /C /I`);
     }
 
-    const resultRemoveBuildFolder = await exec.exec('rmdir /Q /S C:\\_build');
+    const resultRemoveBuildFolder = exec.exec('rmdir /Q /S C:\\_build').then((num) => {
+        console.log(num)
+    });
     console.log(`Result Remove Build Folder: ${resultRemoveBuildFolder}, command: rmdir /Q /S C:\\_build`);
 
-    const resultRemoveAppFolder =  await exec.exec('rmdir /Q /S C:\\App');
+    const resultRemoveAppFolder = exec.exec('rmdir /Q /S C:\\App').then((num) => {
+        console.log(num)
+    });
     console.log(`Result Remove App Folder: ${resultRemoveAppFolder}, command: rmdir /Q /S C:\\App`);
 
     console.log(`Success compiler!`);
